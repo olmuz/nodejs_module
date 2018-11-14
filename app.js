9f34f0ed2c462c57e4d71323f6572fdb03c4774f
oleh <olmuz@outlook.com>
2018-11-15 00:21:44 +0200

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false})); // yield body-parser
 
 app.use('/', (req, res, next) => {
     console.log('intercepts all requests');
     next();
-})
+});
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>The "Add Product" Page</h1>');
-})
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from express</h1>');
-})
+});
 
 app.listen(8000);
