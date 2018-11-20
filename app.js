e97e4dc52c6510370ddeb429d4f8321faec02eaa
oleh <olmuz@outlook.com>
2018-11-20 20:43:14 +0200

    added 404 page

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(shopRoutes);
 
 app.use(bodyParser.urlencoded({extended: false})); // yield body-parser
 
+app.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(8000);
