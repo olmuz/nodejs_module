d394a8e62b87dd63962637728f297bd4da067982
oleh <olmuz@outlook.com>
2018-11-20 22:51:26 +0200

    serving files statically

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const shopRoutes = require('./routes/shop');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: false})); // yield body-parser
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
