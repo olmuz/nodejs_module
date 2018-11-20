669a17e575faa96cc7a870b79e5d22633d359fd9
oleh <olmuz@outlook.com>
2018-11-20 20:36:17 +0200

    using express route

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require("express");
-
-const bodyParser = require("body-parser");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require("body-parser");
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use(bodyParser.urlencoded({extended: false})); // yield body-parser
-
-app.use('/', (req, res, next) => {
-    console.log('intercepts all requests');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
-});
 
 app.listen(8000);
