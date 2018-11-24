52de44ee5ed76ee2b2a3a1a87faa8d7494adcd0c
oleh <olmuz@outlook.com>
2018-11-24 17:05:27 +0200

    sharing data across requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
 
 const bodyParser = require("body-parser");
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false})); // yield body-parser
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,13 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
