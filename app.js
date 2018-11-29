d3ef18ad9cc06523d706e1c89ea84b39a1e3c38b
oleh <olmuz@outlook.com>
2018-11-30 00:31:27 +0200

    using ejs

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
-const expressHbs = require('express-handlebars');
-
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views'); 
 
 const adminData = require("./routes/admin");
@@ -23,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'})
+    res.status(404).render('404', {pageTitle: 'Page not found', path: null})
 });
 
 app.listen(8000);
