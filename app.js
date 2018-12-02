7fff3bfa2539e5ee8218deba26ffbaf2c86839b3
oleh <olmuz@outlook.com>
2018-12-03 00:01:55 +0200

    finishing controllers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('views', 'views');
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const bodyParser = require("body-parser");
 
@@ -16,8 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found', path: null})
-});
+app.use('/', errorController.get404);
 
 app.listen(8000);
