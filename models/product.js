f8881c6decf783725317ff0c2d444cc81a8811ff
oleh <olmuz@outlook.com>
2018-12-03 00:49:13 +0200

    Storing Data in Files using models, using callbacks

--- models/product.js
+++ models/product.js
@@ -1,4 +1,16 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const getProductsFromFile = (cb) => {
+    const p = path.join(rootDir, 'data', 'products.json'); 
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
+};
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +18,26 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(rootDir, 'data', 'products.json'); 
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent)
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(rootDir, 'data', 'products.json'); 
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
-}
+};
