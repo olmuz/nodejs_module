f183cf5a963a0aa086c8b1f21df5b4a03012eeea
oleh <olmuz@outlook.com>
2018-12-17 00:13:09 +0200

    Editing the Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -36,6 +36,23 @@ exports.getEditProduct = (req, res, next) => {
       product: product
     });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedtitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+  console.log(req.body);
+  const updatedProduct = new Product(
+    prodId,
+    updatedtitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice)
+  updatedProduct.save();
+  res.redirect('/admin/products');  
 };
 
 exports.getProducts = (req, res, next) => {
