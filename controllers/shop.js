c77361f56da9fa0fa613dcd79ba503dd17904cfc
oleh <olmuz@outlook.com>
2019-01-05 22:35:17 +0200

    Fetching a Single Product with the where condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,13 +14,15 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  Product.findById(productId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'});
-  });
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then( ([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product[0].title,
+        path: '/products'});
+    })
+    .catch( err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
