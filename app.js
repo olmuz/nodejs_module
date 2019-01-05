7abf9e78285d2ad9d72a80f937192dd13346f8d3
oleh <olmuz@outlook.com>
2019-01-05 21:32:29 +0200

    Connecting the App to SQL Database

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const path = require('path');
-const express = require("express");
 const app = express();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
-const bodyParser = require("body-parser");
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false })); // yield body-parser
 app.use(express.static(path.join(__dirname, 'public')));
