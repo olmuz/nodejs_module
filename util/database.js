673010ab84a0cc3bf60fcb1854b58bf27c8f2a9f
oleh <olmuz@outlook.com>
2019-01-07 17:06:30 +0200

    Sequelize package and connection to the Database

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '1111'
+const sequelize = new Sequelize('node-complete', 'root', '1111', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
