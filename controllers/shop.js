5d41cdc7602ad516197aabe0fcd3d83b52b22f2b
oleh <olmuz@outlook.com>
2019-01-12 18:59:48 +0200

    fix 2

--- controllers/shop.js
+++ controllers/shop.js
@@ -52,16 +52,14 @@ exports.getCart = (req, res, next) => {
   req.user
     .getCart()
     .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err))
+      return cart.getProducts()
+    })
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err))
 };
