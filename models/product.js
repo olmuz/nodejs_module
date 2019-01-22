11a2ca7ae9f23430d954d03f967b92911b542ec9
oleh <olmuz@outlook.com>
2019-01-22 23:12:47 +0200

    Fetching All Products

--- models/product.js
+++ models/product.js
@@ -16,6 +16,20 @@ class Product {
         console.log(result);
       })
       .catch(err => console.log(err))
+  };
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => { 
+        console.log(err);
+      })
   }
 }
 
