3716c176aa27f00c909cc50dea724c84bc877dfe
oleh <olmuz@outlook.com>
2019-01-27 20:46:26 +0200

    Fixing the Additing Product Functionality

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongoDb.ObjectId(id);
+    this._id = id ? new mongoDb.ObjectId(id): null;
   }
 
   save() {
