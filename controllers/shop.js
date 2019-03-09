04e0b302a3f2558d2ef71f3daecdf53d07b46fac
oleh <olmuz@outlook.com>
2019-03-09 19:37:38 +0200

    Storing All Order Related Date

--- controllers/shop.js
+++ controllers/shop.js
@@ -82,7 +82,7 @@ exports.postOrder = (req, res, next) => {
     .then(user => {
       console.log(user.cart.items)
       const products = user.cart.items.map(i => {
-        return {quantity: i.quantity, product: i.productId}
+        return {quantity: i.quantity, product: { ...i.productId._doc }}
       })
       const order = new Order({
         user: {
